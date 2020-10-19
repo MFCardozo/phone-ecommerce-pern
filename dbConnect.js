70ae1e12f4a1e50f0399b709f54ea9faae8710bd
Mcfly6 <manu.mcfly6@gmail.com>
2020-10-19 09:31:44 -0300

    deploy on heroku

--- dbConnect.js
+++ dbConnect.js
@@ -1,13 +1,20 @@
 const path = require("path");
-require("dotenv").config(path.join(__dirname + "/server/.env"));
+require("dotenv").config(path.join(__dirname, "server/.env"));
 
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const devConfig = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-});
+};
+const proConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.node_ENV === "production" ? proConfig : devConfig
+);
 module.exports = pool;
--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 /*/routes*/
 
 app.use(require("./routes/index"));
 
-const listener = app.listen(5000, () => {
+//if GET random url send thee 404
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+const listener = app.listen(PORT, () => {
   console.log("Server listening on port " + listener.address().port);
 });
