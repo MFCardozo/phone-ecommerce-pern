eac7985ac4c52f49f05ffae37a8756e0df27c7d2
Mcfly6 <manu.mcfly6@gmail.com>
2020-10-19 17:40:34 -0300

    fix api endpoint

--- dbConnect.js
+++ dbConnect.js
@@ -14,5 +14,7 @@ const proConfig = {
   connectionString: process.env.DATABASE_URL,
 };
 
-const pool = new Pool(devConfig);
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 module.exports = pool;
